fdcea5fd80b9cbe32bb7d9565983bce686482cf0
Lik Eduard <feft99@gmail.com>
2020-12-03 16:43:10 +0600

    Start project

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    watch: false,
+    entry: path.resolve(__dirname, 'src/ExpertSystemLite.js'),
+    output: {
+        path: path.resolve(__dirname, 'lib'),
+        filename: 'expert-system-lite.min.js'
+        // libraryExport: 'default',
+        // libraryTarget: 'umd',
+        // library: ''
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties', 
+                            '@babel/plugin-proposal-private-methods',
+                            '@babel/plugin-proposal-private-property-in-object'
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}    
+
